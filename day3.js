60a432dfcd117321bfee952a826607f97e8fbaac
unknown <sdgerbich@gmail.com>
2020-04-01 16:25:49 -0400

    async

--- day3.js
+++ day3.js
@@ -1,46 +1,44 @@
-var slideshow= {
-    photoList:["strings go here", "next string", "another one", "one more for good measure"],
-    currentPhotoIndex: 0,
-    nextPhoto: function(){
-        
-        if(this.currentPhotoIndex<this.photoList.length - 1){
-            this.currentPhotoIndex++;
-            console.log(this.photoList[this.currentPhotoIndex])
-            // console.log(this.currentPhotoIndex);
-        }
-        else if (this.currentPhotoIndex<this.photoList.length)
-        {
-            console.log("End Of Slideshow");
-        //    console.log(this.currentPhotoIndex);
-        }
-    },
-    prevPhoto: function(){
-        
-        if(this.currentPhotoIndex>0){
-            this.currentPhotoIndex--;
-             console.log(this.photoList[this.currentPhotoIndex])
-        }
-        else if(this.currentPhotoIndex + 1> 0)
-        {
-            console.log( "Start Of Slideshow");
-            // console.log(this.currentPhotoIndex);
-        }
-    },
-    getCurrentPhoto: function()
-    {
-        console.log(this.photoList[this.currentPhotoIndex]);
+var slideshow = {
+  photoList: [
+    "strings go here",
+    "next string",
+    "another one",
+    "one more for good measure"
+  ],
+  currentPhotoIndex: 0,
+  nextPhoto: function() {
+    if (this.currentPhotoIndex < this.photoList.length - 1) {
+      this.currentPhotoIndex++;
+      console.log(this.photoList[this.currentPhotoIndex]);
+      // console.log(this.currentPhotoIndex);
+    } else if (this.currentPhotoIndex < this.photoList.length) {
+        this.pause();
+      console.log("End Of Slideshow");
+      //    console.log(this.currentPhotoIndex);
     }
-}
-slideshow.nextPhoto();
-slideshow.nextPhoto();
-slideshow.nextPhoto();
-slideshow.nextPhoto();
-slideshow.nextPhoto();
-slideshow.prevPhoto();
-slideshow.prevPhoto();
-slideshow.prevPhoto();
-slideshow.prevPhoto();
-slideshow.prevPhoto();
-slideshow.nextPhoto();
-slideshow.nextPhoto();
-slideshow.getCurrentPhoto();
+  },
+  prevPhoto: function() {
+    if (this.currentPhotoIndex > 0) {
+      this.currentPhotoIndex--;
+      console.log(this.photoList[this.currentPhotoIndex]);
+    } else if (this.currentPhotoIndex + 1 > 0) {
+      console.log("Start Of Slideshow");
+      // console.log(this.currentPhotoIndex);
+    }
+  },
+  getCurrentPhoto: function() {
+    console.log(this.photoList[this.currentPhotoIndex]);
+  },
+  playInterval: null,
+  play: function() {
+    //   console.log(this);
+    var self = this;
+      this.playInterval = setInterval(function() {self.nextPhoto()}, 2000);
+      this.nextPhoto();
+    
+  },
+  pause: function(){
+      clearInterval(this.playInterval);
+  }
+};
+slideshow.play();
